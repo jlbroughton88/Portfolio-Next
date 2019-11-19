fb4c638552dd5a205c3b370554d0a6cf32cd4379
jlb1999 <jlbroughton88@gmail.com>
2019-11-19 08:31:06 -0500

    fixed footer

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7a0aa183c28b6aaa9460";
+/******/ 	var hotCurrentHash = "6a8b2cdc1baa9c3f5fa8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
