b5dda501f87c9db0e3d6be6c713199443e695e2c
jlb1999 <jlbroughton88@gmail.com>
2019-11-20 16:59:39 -0500

    fixed color fade on safari

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1574265483328");
+            injectCss(link, link.href.split("?")[0] + "?unix=1574287061602");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6de066feaae98e5bb0c1";
+/******/ 	var hotCurrentHash = "f752fdf2c88b03403198";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
