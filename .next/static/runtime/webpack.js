c29f343554ec570f25f7ddb819859b57175399c5
jlb1999 <jlbroughton88@gmail.com>
2019-11-20 11:16:08 -0500

    cleaned up story CSS

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7ce8038c64588370fb8a";
+/******/ 	var hotCurrentHash = "6de066feaae98e5bb0c1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
