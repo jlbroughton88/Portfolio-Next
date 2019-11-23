90faeb603634bbba7706fcef09bc4abc85727784
jlb1999 <jlbroughton88@gmail.com>
2019-11-23 10:42:21 -0500

    changed nav and footer styles, made social icons a little smaller

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1574522065505");
+            injectCss(link, link.href.split("?")[0] + "?unix=1574523712199");
           });
       }
     }
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1574514273922");
+            injectCss(link, link.href.split("?")[0] + "?unix=1574523253997");
           });
       }
     }
@@ -140,7 +140,44 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1574360641254");
+            injectCss(link, link.href.split("?")[0] + "?unix=1574522884295");
+          });
+      }
+    }
+  
+
+/***/ }),
+
+/***/ "./public/css/story.scss":
+/*!*******************************!*\
+  !*** ./public/css/story.scss ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1574517637153");
           });
       }
     }
---
+++ .next/static/webpack/styles.21c29b63722ce8087485.hot-update.js
@@ -0,0 +1,16 @@
+webpackHotUpdate("styles",{
+
+/***/ "./public/css/story.scss":
+false,
+
+/***/ "./public/css/work.scss":
+false,
+
+/***/ 0:
+false,
+
+/***/ 4:
+false
+
+})
+//# sourceMappingURL=styles.21c29b63722ce8087485.hot-update.js.map
