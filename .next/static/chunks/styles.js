d9020da2ad074765905f3d02cbb97f1404959fd0
jlb1999 <jlbroughton88@gmail.com>
2019-11-27 15:13:24 -0500

    some font sizes changed for a better

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -66,7 +66,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1574882912393");
+            injectCss(link, link.href.split("?")[0] + "?unix=1574885215151");
           });
       }
     }
@@ -103,7 +103,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1574883044629");
+            injectCss(link, link.href.split("?")[0] + "?unix=1574883663562");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f9b42758ea5b82415680";
+/******/ 	var hotCurrentHash = "adfa8603dac0259df500";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
