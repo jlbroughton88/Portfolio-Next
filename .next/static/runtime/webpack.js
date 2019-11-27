152dfdff0224d58f23bb390a7cbd710e28f6efcd
jlb1999 <jlbroughton88@gmail.com>
2019-11-27 06:31:20 -0500

    hover effects on work examples

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "92ec5ec3daa6bd1e1572";
+/******/ 	var hotCurrentHash = "5cebb45665fec37a4744";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
