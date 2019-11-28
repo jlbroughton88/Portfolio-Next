80f272260ebbc3ca07b6b6de30f9628df1849d63
jlb1999 <jlbroughton88@gmail.com>
2019-11-28 07:57:38 -0500

    cleaned up css and fixed spacing on mobile

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fc2379d6009497389d57";
+/******/ 	var hotCurrentHash = "d26306b7f5c25d6f93f5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
