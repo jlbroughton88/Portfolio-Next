b770018a53089adad9031d6d1b4f25d1be1f8c6f
jlb1999 <jlbroughton88@gmail.com>
2019-12-04 20:45:48 -0500

    adjusted svg and intro p

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e70d128a6c37223d75f9";
+/******/ 	var hotCurrentHash = "730e081c9d29d2c2be61";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
