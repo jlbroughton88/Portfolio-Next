9e1d9e90ead2ba43c697548d93ddf20c1e9cff7b
jlb1999 <jlbroughton88@gmail.com>
2019-12-04 19:50:26 -0500

    added animation to arrow

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0d453003c277d822944f";
+/******/ 	var hotCurrentHash = "e70d128a6c37223d75f9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
